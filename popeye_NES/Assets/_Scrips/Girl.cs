2c45fcb4256571323b15f165d49ad73c202eeb5b
Badr eldeen <badrmohamed86864@gmail.com>
2022-06-02 21:51:35 +0200

    Girl-setup-01
    adding way Points to Girl-01

--- popeye_NES/Assets/_Scrips/Girl.cs
+++ popeye_NES/Assets/_Scrips/Girl.cs
@@ -7,19 +7,45 @@ public class Girl : MonoBehaviour
     Rigidbody2D rb;
     //-----------------------
     [SerializeField] private float _speed;
+    [SerializeField] private int randomSpot;
     //-----------------------
     private Vector2 _movement;
 
     //-----------------------
+    [SerializeField] Transform[] wayPoints;
+    //-----------------------
+ 
 
     void Start()
     {
-        
+        randomSpot = Random.Range(0, wayPoints.Length);
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        _movement = new Vector2( 1* _speed,0);
+        FollowWayPoints();
+    }
+
+    private void FixedUpdate()
+    {
         
+        Movement();
+    }
+    void FollowWayPoints()
+    {
+
+
+        _movement = Vector2.MoveTowards(transform.position, wayPoints[randomSpot].transform.position, _speed);
+
+
+
+    }
+
+    void Movement()
+    {
+        rb.AddForce(-_movement, ForceMode2D.Impulse);
     }
 }
